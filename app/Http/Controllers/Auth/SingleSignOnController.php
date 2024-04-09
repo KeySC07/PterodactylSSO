b93bdf30a1dcb411e865cddcddd988cbb1c7d4a1
Key <scarlson6603@gmail.com>
2024-04-10 03:37:43 +0930

    Adding proper handling of the firstname and lastname as well as adding support for admin based on the group in Authentik

--- app/Http/Controllers/Auth/SingleSignOnController.php
+++ app/Http/Controllers/Auth/SingleSignOnController.php
@@ -34,18 +34,29 @@ class SingleSignOnController extends AbstractLoginController
     private function CreateNewAccount($service)
     {
         $email = $service->getEmail();
-        if(!$service->getNickname()) { $username =  $service->getName(); } else { $username =  $service->getNickname(); }
+        $username = !$service->getNickname() ? $service->getName() : $service->getNickname();
         
+        // Normalize username by removing spaces and non-alphanumeric characters
         $username = str_replace(' ', '', $username);
         $username = preg_replace("/[^a-zA-Z0-9]+/", "", $username);
-
-
-        //TODO Add admin auth based on user groups: $userGroups = $service->groups;
-
-        $res = Artisan::call("p:user:make --email={$email} --username={$username} --name-first={$username} --name-last={$username} --admin=0 --no-password");
-        $getUser = User::where('email', $service->getEmail())->first();
+    
+        // Assuming the OAuth token includes attributes for groups, first name, and last name
+        // You'll need to replace 'groups', 'firstName', and 'lastName' with the actual attribute names
+        $userGroups = $service->groups;
+        list($firstName, $lastName) = explode(' ', $service->getName(), 2);
+    
+        // Determine if the user should be an admin based on their groups
+        $isAdmin = 0; // Default to non-admin
+        if (in_array('Pterodactyl Admins', $userGroups)) { // Adjust 'admin' to match the actual admin group name
+            $isAdmin = 1;
+        }
+    
+        // Replace "--name-first={$username} --name-last={$username}" with actual first name and last name
+        // and "--admin=0" with "$isAdmin" to set admin rights based on group membership
+        $res = Artisan::call("p:user:make --email={$email} --username={$username} --name-first={$firstName} --name-last={$lastName} --admin={$isAdmin} --no-password");
+    
+        $getUser = User::where('email', $email)->first();
         Auth::loginUsingId($getUser->id);
-
     }
 
 
